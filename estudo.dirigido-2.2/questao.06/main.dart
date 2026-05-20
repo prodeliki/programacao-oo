c5d7b5451869cca0008782b9e420fd64c759e843
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:13:32 -0300

    Add main function to calculate ticket prices

--- estudo.dirigido-2.2/questao.06/main.dart
+++ estudo.dirigido-2.2/questao.06/main.dart
@@ -1,1 +1,7 @@
+void main() {
+  IngressoVip vip = IngressoVip('Show Sabrina Carpenter', 100.0, 50.0);
+  IngressoPromocional promo = IngressoPromocional('Cinema', 40.0, 10.0);
 
+  print('Valor VIP: R\$ ${vip.valorFinal()}');
+  print('Valor Promo: R\$ ${promo.valorFinal()}');
+}
