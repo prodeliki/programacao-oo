03410eea49121b3d986b32ebef06abec561cd372
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:08:59 -0300

    Update conta.bancaria.dart

--- estudo.dirigido-2.2/questao.04/conta.bancaria.dart
+++ estudo.dirigido-2.2/questao.04/conta.bancaria.dart
@@ -1,1 +1,13 @@
+class ContaBancaria {
+  String titular;
+  double saldo;
+
+  ContaBancaria(this.titular, this.saldo);
+
+  void depositar(double valor) => saldo += valor;
+  void sacar(double valor) => saldo -= valor;
+  void exibirSaldo() => print('Titular: $titular - Saldo: R\$ $saldo');
+}
+
+
 
