4867b0f47708ec015ac29f733a9fc1b1b0e384e8
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:12:41 -0300

    Update ingresso.promocional.dart

--- estudo.dirigido-2.2/questao.06/ingresso.promocional.dart
+++ estudo.dirigido-2.2/questao.06/ingresso.promocional.dart
@@ -1,1 +1,9 @@
+class IngressoPromocional extends Ingresso implements Calculavel {
+  double desconto;
+
+  IngressoPromocional(String evento, double valor, this.desconto) : super(evento, valor);
+
+  @override
+  double valorFinal() => valor - desconto;
+}
 
