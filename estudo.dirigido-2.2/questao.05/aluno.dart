68bb126dd5438134a0aeb8cd437ed09353345dea
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:10:40 -0300

    Update aluno.dart

--- estudo.dirigido-2.2/questao.05/aluno.dart
+++ estudo.dirigido-2.2/questao.05/aluno.dart
@@ -1,1 +1,14 @@
+class Aluno extends Pessoa {
+  String matricula;
+  String curso;
 
+  Aluno(String nome, int idade, this.matricula, this.curso) : super(nome, idade);
+
+  @override
+  void exibirDados() {
+    print('Nome: $nome');
+    print('Idade: $idade');
+    print('Matricula: $matricula');
+    print('Curso: $curso');
+  }
+}
