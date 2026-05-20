b3736c610c27fb00bc30a83c3b55c0c152f6d7ef
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:18:29 -0300

    Update main.dart

--- estudo.dirigido-2.2/questao.08/main.dart
+++ estudo.dirigido-2.2/questao.08/main.dart
@@ -1,1 +1,7 @@
+void main() {
+  LivroDigital digital = LivroDigital('Dart Iniciante', 'Betina', 1.5);
+  LivroFisico fisico = LivroFisico('POO Avançado', 'Chaua', 300);
 
+  digital.exibirInformacoes();
+  fisico.exibirInformacoes();
+}
