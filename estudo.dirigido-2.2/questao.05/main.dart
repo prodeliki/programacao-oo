8afa2fddf36d2175991a124ddea76e993bd603f4
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:10:53 -0300

    Update main.dart

--- estudo.dirigido-2.2/questao.05/main.dart
+++ estudo.dirigido-2.2/questao.05/main.dart
@@ -1,1 +1,7 @@
+void main() {
+  Aluno aluno = Aluno('Betina', 18, '123-A', 'Sistemas');
+  Professor professor = Professor('Chaua', 35, 'POO', 5000.0);
 
+  aluno.exibirDados();
+  professor.exibirDados();
+}
