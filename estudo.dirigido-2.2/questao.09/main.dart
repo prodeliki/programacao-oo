1b6d69e7d1bfe21261119f2247f435bf264982ad
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:19:12 -0300

    Add main function to display delivery and pickup orders

--- estudo.dirigido-2.2/questao.09/main.dart
+++ estudo.dirigido-2.2/questao.09/main.dart
@@ -1,1 +1,7 @@
+void main() {
+  PedidoDelivery p1 = PedidoDelivery(101, 50.0, 'Rua A, 123', 7.0);
+  PedidoRetirada p2 = PedidoRetirada(102, 35.0, 'Carlos');
 
+  p1.exibirPedido();
+  p2.exibirPedido();
+}
