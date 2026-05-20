4bee353e1b96aa1d1eca64e0b91747231a397bb3
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:09:40 -0300

    Implement interest application in ContaPoupanca class

--- estudo.dirigido-2.2/questao.04/conta.poupanca.dart
+++ estudo.dirigido-2.2/questao.04/conta.poupanca.dart
@@ -1,1 +1,9 @@
+class ContaPoupanca extends ContaBancaria implements Rentavel {
+  ContaPoupanca(String titular, double saldo) : super(titular, saldo);
+
+  @override
+  void aplicarRendimento() {
+    saldo += saldo * 0.1;
+  }
+}
 
