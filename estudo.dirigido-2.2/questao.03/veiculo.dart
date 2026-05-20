1d8413ebb723768c4d0503edd9df55f6cadaf0fe
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:08:32 -0300

    Add abstract class Veiculo and subclasses Carro and Moto

--- estudo.dirigido-2.2/questao.03/veiculo.dart
+++ estudo.dirigido-2.2/questao.03/veiculo.dart
@@ -1,1 +1,38 @@
+abstract class Veiculo {
+  String marca;
+  String modelo;
+  int ano;
+
+  Veiculo(this.marca, this.modelo, this.ano);
+
+  void exibirFicha();
+}
+
+class Carro extends Veiculo {
+  int quantidadePortas;
+
+  Carro(String marca, String modelo, int ano, this.quantidadePortas) : super(marca, modelo, ano);
+
+  @override
+  void exibirFicha() {
+    print('Marca: $marca');
+    print('Modelo: $modelo');
+    print('Ano: $ano');
+    print('Numero de portas: $quantidadePortas');
+  }
+}
+
+class Moto extends Veiculo {
+  int cilindradas;
+
+  Moto(String marca, String modelo, int ano, this.cilindradas) : super(marca, modelo, ano);
+
+  @override
+  void exibirFicha() {
+    print('Marca: $marca');
+    print('Modelo: $modelo');
+    print('Ano: $ano');
+    print('Cilindradas: $cilindradas');
+}
+
 
