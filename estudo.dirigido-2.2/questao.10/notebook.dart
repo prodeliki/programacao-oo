45f3917721d25394bddf952334c280890de591fb
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:21:06 -0300

    Add Notebook class implementing Ligavel interface

--- estudo.dirigido-2.2/questao.10/notebook.dart
+++ estudo.dirigido-2.2/questao.10/notebook.dart
@@ -1,1 +1,11 @@
+class Notebook extends Dispositivo implements Ligavel {
+  int memoriaRam;
 
+  Notebook(String marca, String modelo, this.memoriaRam) : super(marca, modelo);
+
+  @override
+  void ligar() => print('Notebook $marca ligando com $memoriaRam GB RAM...');
+
+  @override
+  void desligar() => print('Notebook $marca desligando...');
+}
