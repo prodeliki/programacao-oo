12b01c7d09ccb7e01eb67cb19317a20dc0c45ca9
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:11:29 -0300

    Add Professor class extending Pessoa with attributes

--- estudo.dirigido-2.2/questao.05/professor.dart
+++ estudo.dirigido-2.2/questao.05/professor.dart
@@ -1,1 +1,15 @@
+class Professor extends Pessoa {
+  String disciplina;
+  double salario;
+
+  Professor(String nome, int idade, this.disciplina, this.salario) : super(nome, idade);
+
+  @override
+  void exibirDados() {
+    print('Nome: $nome');
+    print('Idade: $idade');
+    print('Disciplina: $disciplina');
+    print('Salário: $salario');
+  }
+}
 
