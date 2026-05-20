2626db76b9c2187143c380cf3e54e8dbde7ab109
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:17:23 -0300

    Add LivroDigital class to handle digital books

--- estudo.dirigido-2.2/questao.08/digital.dart
+++ estudo.dirigido-2.2/questao.08/digital.dart
@@ -1,1 +1,11 @@
+class LivroDigital extends Livro implements Informativo {
+  double tamanhoArquivo;
+
+  LivroDigital(String titulo, String autor, this.tamanhoArquivo) : super(titulo, autor);
+
+  @override
+  void exibirInformacoes() {
+    print('Digital: $titulo - $autor (Tamanho: $tamanhoArquivo MB)');
+  }
+}
 
