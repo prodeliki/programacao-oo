d0c293c07bc6d8c72da270e02e5853b625826ac2
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:09:18 -0300

    Update conta.corrente.dart

--- estudo.dirigido-2.2/questao.04/conta.corrente.dart
+++ estudo.dirigido-2.2/questao.04/conta.corrente.dart
@@ -1,1 +1,8 @@
+class ContaCorrente extends ContaBancaria implements Rentavel {
+  ContaCorrente(String titular, double saldo) : super(titular, saldo);
 
+  @override
+  void aplicarRendimento() {
+    saldo += saldo * 0.5;
+  }
+}
