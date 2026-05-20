cc94e57b8c9626965224f06feb41ece0335a09d2
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:17:39 -0300

    Update fisico.dart

--- estudo.dirigido-2.2/questao.08/fisico.dart
+++ estudo.dirigido-2.2/questao.08/fisico.dart
@@ -1,1 +1,11 @@
+class LivroFisico extends Livro implements Informativo {
+  int quantidadePaginas;
+
+  LivroFisico(String titulo, String autor, this.quantidadePaginas) : super(titulo, autor);
+
+  @override
+  void exibirInformacoes() {
+    print('Fisico: $titulo - $autor ($quantidadePaginas páginas)');
+  }
+}
 
