51787e8ed5fa47d2a35f7f9945c671b15a6202c1
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:13:01 -0300

    Add IngressoVip class with additional value calculation

--- estudo.dirigido-2.2/questao.06/ingresso.vip.dart
+++ estudo.dirigido-2.2/questao.06/ingresso.vip.dart
@@ -1,1 +1,8 @@
+class IngressoVip extends Ingresso implements Calculavel {
+  double valorAdicional;
 
+  IngressoVip(String evento, double valor, this.valorAdicional) : super(evento, valor);
+
+  @override
+  double valorFinal() => valor + valorAdicional;
+}
