520d51720505336cf1780400a315ded3598d32b3
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:14:54 -0300

    Implement main function to create animal instances

--- estudo.dirigido-2.2/questao.07/main.dart
+++ estudo.dirigido-2.2/questao.07/main.dart
@@ -1,1 +1,11 @@
+void main() {
+  Cachorro cachorro = Cachorro('Lua');
+  Gato gato = Gato('Tufao');
+  Passaro passaro = Passaro('Luan');
+  Vaca vaca = Vaca('Olivia');
 
+  cachorro.emitirSom();
+  gato.emitirSom();
+  passaro.emitirSom();
+  vaca.emitirSom();
+}
