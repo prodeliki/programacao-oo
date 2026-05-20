5c402b85f3674831f60e23120ac199ae87cd9b0f
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:18:52 -0300

    Add PedidoDelivery class for delivery orders

--- estudo.dirigido-2.2/questao.09/delivery.dart
+++ estudo.dirigido-2.2/questao.09/delivery.dart
@@ -1,1 +1,13 @@
+class PedidoDelivery extends Pedido {
+  String enderecoEntrega;
+  double taxaEntrega;
+
+  PedidoDelivery(int codigo, double valor, this.enderecoEntrega, this.taxaEntrega) : super(codigo, valor);
+
+  @override
+  void exibirPedido() {
+    double total = valorTotal + taxaEntrega;
+    print('Pedido #$codigo - Entrega em: $enderecoEntrega - Total: R\$ $total');
+  }
+}
 
