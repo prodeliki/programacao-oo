cfd8562cd7072890db0aa4f3d8b078edef499b88
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:08:05 -0300

    Update main.dart

--- estudo.dirigido-2.2/questao.03/main.dart
+++ estudo.dirigido-2.2/questao.03/main.dart
@@ -1,1 +1,7 @@
+void main() {
+  Carro meuCarro = Carro('Toyota', 'Kwid', 2022, 4);
+  Moto minhaMoto = Moto('Honda', 'CB 200', 2023, 500);
 
+  meuCarro.exibirFicha();
+  minhaMoto.exibirFicha();
+}
