30579a975d79abf9a26167d40f13bf0783a1f861
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:19:50 -0300

    Add PedidoRetirada class for withdrawal orders

--- estudo.dirigido-2.2/questao.09/retirada.dart
+++ estudo.dirigido-2.2/questao.09/retirada.dart
@@ -1,1 +1,11 @@
+class PedidoRetirada extends Pedido {
+  String nomeCliente;
+
+  PedidoRetirada(int codigo, double valor, this.nomeCliente) : super(codigo, valor);
+
+  @override
+  void exibirPedido() {
+    print('Pedido #$codigo - Retirada por: $nomeCliente - Valor: R\$ $valorTotal');
+  }
+}
 
