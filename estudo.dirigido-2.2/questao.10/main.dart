5a859d05a3045f3f2aa7445cc11603ff54a4d10e
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:20:52 -0300

    Add main function to initialize devices and call ligar

--- estudo.dirigido-2.2/questao.10/main.dart
+++ estudo.dirigido-2.2/questao.10/main.dart
@@ -1,1 +1,7 @@
+void main() {
+  Smartphone smartphone = Smartphone('Apple', 'Iphone 12', 'IOS');
+  Notebook notebook = Notebook('Dell', 'Inspirion', 16);
 
+  smartphone.ligar();
+  notebook.ligar();
+}
