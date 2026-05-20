144ebb967717eec4c3f4bf8bbbfcfacd8bd8bdf9
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:21:23 -0300

    Update smartphone.dart

--- estudo.dirigido-2.2/questao.10/smartphone.dart
+++ estudo.dirigido-2.2/questao.10/smartphone.dart
@@ -1,1 +1,11 @@
+class Smartphone extends Dispositivo implements Ligavel {
+  String sistemaOperacional;
 
+  Smartphone(String marca, String modelo, this.sistemaOperacional) : super(marca, modelo);
+
+  @override
+  void ligar() => print('Smartphone $marca $modelo ligando...');
+
+  @override
+  void desligar() => print('Smartphone $marca desligando...');
+}
