9bfb7cad1fe8d9af67588975dc924b4bcd30a2d9
Betina Cardoso <betinahigh@gmail.com>
2026-05-20 14:10:02 -0300

    Implement main function to manage accounts

--- estudo.dirigido-2.2/questao.04/main.dart
+++ estudo.dirigido-2.2/questao.04/main.dart
@@ -1,1 +1,10 @@
+void main() {
+  ContaPoupanca poupanca = ContaPoupanca('Betina', 1000.0);
+  ContaCorrente corrente = ContaCorrente('Chaua', 1000.0);
 
+  poupanca.aplicarRendimento();
+  corrente.aplicarRendimento();
+
+  poupanca.exibirSaldo();
+  corrente.exibirSaldo();
+}
